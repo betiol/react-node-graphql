29b255270500abd4f857d37e0d80fbd694c88642
Nikollas Betiol <nikollasbetiol@gmail.com>
2017-06-05 12:24:00 -0400

    add company association

--- schema/schema.js
+++ schema/schema.js
@@ -8,12 +8,28 @@ const {
 } = graphql;
 
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+})
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                    .then(resp => resp.data);
+            }
+        }
     }
 });
 
