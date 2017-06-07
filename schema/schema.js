33aae60ec053be61f77aa957c16e438ce958ffa6
Nikollas Betiol <nikollasbetiol@gmail.com>
2017-06-07 09:17:34 -0400

    Add editUser on mutation

--- schema/schema.js
+++ schema/schema.js
@@ -99,6 +99,19 @@ const mutation = new GraphQLObjectType({
                 return axios.delete(`http://localhost:3000/users/${id}`)
                     .then(res => res.data);
             }
+        },
+        editUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) },
+                firstName: { type: GraphQLString },
+                age: { type: GraphQLInt },
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+                    .then(res => res.data);
+            }
         }
     }
 });
